9d67b1040deb5360618d46676200449e11eb6a0e
Artex154 <a.stockelynck12@outlook.com>
2025-08-16 14:27:00 +0200

    Initial commit

---
+++ src/main/java/be/artex/permafrost/PermafrostDataGenerator.java
@@ -0,0 +1,11 @@
+package be.artex.permafrost;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PermafrostDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
